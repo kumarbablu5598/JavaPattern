493f5a2568e4985e61b73394b54dbe9617b1e020
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-21 23:45:39 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,49 @@
+Take Integer N as input and print the following pattern.
+    
+I/p  N=5
+
+O/P
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+I/P N=6
+    
+O/p   
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *   
+* * * * * *   
+
+// java code
+
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j<=i)
+                System.out.print("* ");
+                else 
+                System.out.print(" ");
+            }
+            System.out.println();
+             
+        }
+    }
+}
