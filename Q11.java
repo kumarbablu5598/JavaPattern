a91d6f419df8a2c006176dc999fdd0d41936b43f
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-24 00:28:51 +0530

    Update Q11.java

--- Q11.java
+++ Q11.java
@@ -3,4 +3,39 @@ Take Integer N as input and print the following pattern.
     
 I/p  N=5
 
-O/P
+O/P 	 	 	 	 	*		
+ 	 	 	 	 	*		*		
+ 	 	 	 	*		*		*		
+ 	 	 	*		*		*		*		
+ 	 	*		*		*		*		*	
+I/p N =7 
+O/p                            
+       
+                                *		
+ 	 	 	 	 	 	 	*		*		
+ 	 	 	 	 	 	*		*		*		
+ 	 	 	 	 	*		*		*		*		
+ 	 	 	 	*		*		*		*		*		
+ 	 	 	*		*		*		*		*		*		
+ 	 	*		*		*		*		*		*		*    
+
+
+    // java Code 
+import java.io.*; 
+import java.util.*; 
+public class Main {
+    public static void main(String[] args) {
+      
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         for(int i=0;i<n;i++){
+             for(int j=0;j<=n-i;j++){
+                 System.out.print(" \t");
+             }
+             for(int j=0;j<=i;j++){
+                 System.out.print("*\t\t");
+             }
+              System.out.println();
+         }
+    }
+}
