598241848af325d954a3870d3514c7dae5f55115
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-24 00:50:16 +0530

    Update Q12.java

--- Q12.java
+++ Q12.java
@@ -1,1 +1,22 @@
+Take Integer N as input and print the following pattern.
+    
+I/p  N=5
 
+O/P  
+
+                             
+ 	 	 	 	*
+ 	 	 	*	!	*
+ 	 	*	!	*	!	*
+ 	*	!	*	!	*	!	*
+*	!	*	!	*	!	*	!	*
+
+  I/P N=7
+  O/P 
+            *
+ 	 	 	 	 	*	!	*
+ 	 	 	 	*	!	*	!	*
+ 	 	 	*	!	*	!	*	!	*
+ 	 	*	!	*	!	*	!	*	!	*
+ 	*	!	*	!	*	!	*	!	*	!	*
+*	!	*	!	*	!	*	!	*	!	*	!	*
