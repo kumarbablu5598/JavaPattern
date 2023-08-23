358bb47cba6d3a95ee4013147ec1f481c98c3e75
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-24 00:51:26 +0530

    Update Q12.java

--- Q12.java
+++ Q12.java
@@ -10,13 +10,22 @@ O/P
  	 	*	!	*	!	*
  	*	!	*	!	*	!	*
 *	!	*	!	*	!	*	!	*
+ // java Code 
 
-  I/P N=7
-  O/P 
-         	 	 	 	*
- 	 	 	 	 	*	!	*
- 	 	 	 	*	!	*	!	*
- 	 	 	*	!	*	!	*	!	*
- 	 	*	!	*	!	*	!	*	!	*
- 	*	!	*	!	*	!	*	!	*	!	*
-*	!	*	!	*	!	*	!	*	!	*	!	*
+    public class Main {
+    public static void main(String[] args) {
+      
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         for(int i=1;i<=n;i++){
+             for(int j=0;j<=n-i-1;j++){
+                 System.out.print(" \t");
+             }
+             for(int j=0;j<i-1;j++){
+                 System.out.print("*\t!\t");
+             }
+             
+              System.out.println("*");
+         }
+    }
+}
