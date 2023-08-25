e94e97bd364515cea0ebdcc36019dad2462010f3
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-26 01:14:28 +0530

    Update Q24.java

--- Q24.java
+++ Q24.java
@@ -3,4 +3,31 @@ Take Integer N as input and print the following pattern.
 I/p  N=5
 
 O/P
-  
+          1         
+        2 2 2       
+      3 3 3 3 3     
+    4 4 4 4 4 4 4   
+  5 5 5 5 5 5 5 5 5 
+    
+  // java Code 
+    public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=2*n-1;j++) 
+            {   
+                 if(j>n-i && j<n+i){
+                      System.out.print(i+" ");
+                    
+                 }
+               
+                else 
+                System.out.print("  ");
+                
+            }  
+          System.out.println();   
+        }
+    }
+}
