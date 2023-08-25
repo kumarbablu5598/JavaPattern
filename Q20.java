8fcba257dde99f50a6160602a6809a22258f88cf
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-26 00:01:50 +0530

    Update Q20.java

--- Q20.java
+++ Q20.java
@@ -1,5 +1,45 @@
 Take Integer N as input and print the following pattern.
     
-I/p  N=5
+I/p  N=7
 
 O/P
+
+      *       
+    *   *     
+  *       *   
+*           * 
+  *       *   
+    *   *     
+      *   
+// java Code 
+    public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {  
+                int T= n/2 ;
+             if(i< T)
+              {        
+                if(j==T-i ||j==T+i)
+                     System.out.print("* ");
+                 else 
+                     System.out.print("  ");
+              }  
+             else 
+             {
+                 int p=n-1-i;
+                 if(j== T-p || j== T+p )
+                     System.out.print("* ");
+                 else 
+                     System.out.print("  ");
+             }   
+                    
+            }
+            System.out.println();
+             
+        }
+    }
+}
