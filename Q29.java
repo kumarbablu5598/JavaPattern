d167f05fec78921f1f04c5855e8c30284f696038
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-26 01:26:52 +0530

    Update Q29.java

--- Q29.java
+++ Q29.java
@@ -3,3 +3,33 @@ Take Integer N as input and print the following pattern.
 I/p  N=5
 
 O/P
+        1         
+      2 0 2       
+    3 0 0 0 3     
+  4 0 0 0 0 0 4   
+5 0 0 0 0 0 0 0 5 
+
+
+    // java Code 
+    public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<2*n-1;j++) 
+            {
+                 if(j>=n-1-i && j<=n-1+i)
+                 {
+                     if(j==n-1-i || j==n-1+i) System.out.print(i+1 +" ");
+                     else  System.out.print("0 ");
+                 }
+                
+                else 
+                System.out.print("  ");
+                
+            }  
+          System.out.println();   
+        }
+    }
+}
