74b072a3208f970c3c7cc62661b2cd0085d4f48a
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-25 23:29:20 +0530

    Update Q17.java

--- Q17.java
+++ Q17.java
@@ -5,10 +5,43 @@ I/p  N=7
 
 O/P
 
-        * * *   * * * 
+* * *   * * * 
 * *       * * 
 *           * 
               
 *           * 
 * *       * * 
 * * *   * * * 
+// java code 
+    public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {  
+                
+               
+              if(i<n/2)
+              {        
+                if(j>= (n/2)-i && j<=(n/2)+i)
+                     System.out.print("  ");
+                 else 
+                     System.out.print("* ");
+              }  
+             else 
+             {
+                 int p=n-1-i;
+                 if(j>=(n/2)-p && j<=(n/2)+p)
+                     System.out.print("  ");
+                 else 
+                     System.out.print("* ");
+             }   
+                    
+            }
+            System.out.println();
+             
+        }
+    }
+}
