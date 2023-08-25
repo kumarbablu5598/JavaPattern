e67072274cc1dc1b405c70cf73792ba10d2f50d0
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2023-08-26 00:50:53 +0530

    Update Q22.java

--- Q22.java
+++ Q22.java
@@ -3,3 +3,32 @@ Take Integer N as input and print the following pattern.
 I/p  N=5
 
 O/P
+    
+* * * * * *  *  *  *  
+* * * *   *  *  *  *  
+* * *        *  *  *  
+* *             *  *  
+*                  *  
+// java code
+    public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<2*n-1;j++) 
+            {
+                 if(j<n){
+                     if(j<=n-1-i) System.out.print("* ");
+                         else System.out.print("  ");
+                 }
+                else{
+                    int p= 2*n-2-j;
+                    if(p<=n-1-i)System.out.print("*  ");
+                         else System.out.print("   ");
+                }
+            }  
+          System.out.println();   
+        }
+    }
+}
